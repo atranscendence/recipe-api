914c8945880774db88ef119ca181d193e51c17a7
Herdre <herdre@myhost>
2020-10-11 02:52:21 -0700

    Configure postgres DB and tests for waiting connetions, added command to wait for connection

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,15 @@ LABEL key="juudgespy@gmail.com"
 ENV PYTHONUNBUFFRED 1
 
 COPY ./requirements.txt /requirements.txt
+# Instaling temp req for postgresql and save them as tmp-build-deps
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev
+
 RUN pip install -r /requirements.txt
+
+# Delete temp req to keep everhting tidy
+RUN apk del .tmp-build-deps 
 
 RUN mkdir /app-api
 WORKDIR /app-api
---
+++ app-api/main_app/tests/test_commands.py
@@ -0,0 +1,27 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_read(self):
+        """Test waiting_for_db  when db is already avalible
+         to connect online ones"""
+
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            # wait_for_db or function to wait for connection
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    # decoretor overites time sleep to return True for faster test
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test waiting_for_db when db is not avalible"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError]*5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
